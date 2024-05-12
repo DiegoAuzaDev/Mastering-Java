6beb2373c2bcf12aefc110f98363974bf4d2b044
diegoauza <auzajuan1@gmail.com>
2024-05-12 15:12:46 -0400

    Variable Scopes

---
+++ src/Challenges/MinutesToYearsAndDays.java
@@ -0,0 +1,14 @@
+package Challenges;
+
+public class MinutesToYearsAndDays {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void printYearsAndDays (long minutes){
+        // calculates years and minutes from the minutes parameter
+
+    }
+
+}

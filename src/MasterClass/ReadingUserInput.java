5dbdb2e65ef8979dc228994ac0f0b90fcf7797f6
diegoauza <auzajuan1@gmail.com>
2024-05-13 10:41:39 -0400

    Exception handling, Adding scanner

--- src/MasterClass/ReadingUserInput.java
+++ src/MasterClass/ReadingUserInput.java
@@ -1,14 +1,26 @@
+
 package MasterClass;
+import java.util.*;
 
 public class ReadingUserInput {
 
     public static void main(String[] args) {
-
         int currentYear = 2024;
-        String userDateOfBirth = "2004";
-        int dateOfBirth = Integer.parseInt(userDateOfBirth);
+        System.out.println(getInputFromConsole(currentYear));
+    }
 
-        System.out.println("Age = " + (currentYear - dateOfBirth));
+    public static String getInputFromConsole (int currentYear){
+        String name = System.console().readLine("Hi, what is your name?");
+        System.out.printf(" Hi, %s welcome to mastering Java with Diego and thank you for taking the time to run this program%n", name);
+        String dateOfBirthDay = System.console().readLine("What year were you born?");
+        int age = currentYear - Integer.parseInt(dateOfBirthDay);
+
+        return "%s, you are %d years old".formatted(name, age);
+    };
+
+    public static String getInputFromScanner (int currentYear){
+
+        return "";
     }
 
 }

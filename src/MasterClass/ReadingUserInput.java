2b54753539e2fc39e7cb6f39b93ad1fb104567a6
diegoauza <auzajuan1@gmail.com>
2024-05-13 11:03:25 -0400

    Testing for invalid user input such as negative int

--- src/MasterClass/ReadingUserInput.java
+++ src/MasterClass/ReadingUserInput.java
@@ -6,7 +6,14 @@ public class ReadingUserInput {
 
     public static void main(String[] args) {
         int currentYear = 2024;
-        System.out.println(getInputFromConsole(currentYear));
+
+        // this code throws an error on IntelliJ
+        try{
+            System.out.println(getInputFromConsole(currentYear));
+        } catch (Exception e){
+            System.out.println("You can not run console readLine on IntelliJ");
+        }
+            System.out.println(getInputFromScanner(currentYear));
     }
 
     public static String getInputFromConsole (int currentYear){
@@ -14,13 +21,20 @@ public class ReadingUserInput {
         System.out.printf(" Hi, %s welcome to mastering Java with Diego and thank you for taking the time to run this program%n", name);
         String dateOfBirthDay = System.console().readLine("What year were you born?");
         int age = currentYear - Integer.parseInt(dateOfBirthDay);
-
         return "%s, you are %d years old".formatted(name, age);
     };
 
     public static String getInputFromScanner (int currentYear){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hello there, please enter your name ");
 
-        return "";
+        String userName = scanner.nextLine();
+
+        System.out.printf("Welcome %s, enter your birth year to calculate your age%n", userName);
+
+        int userAge = scanner.nextInt();
+
+        return "%s, you are %d years old".formatted(userName, (currentYear - userAge));
     }
 
 }

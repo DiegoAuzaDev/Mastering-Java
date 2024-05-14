56a5467202407510047ac98bacaa1ed3aaa659b9
diegoauza <auzajuan1@gmail.com>
2024-05-14 16:52:18 -0400

    Nascissit Number checker add

---
+++ src/Challenges/NarcissisticNumber.java
@@ -0,0 +1,25 @@
+package Challenges;
+
+public class NarcissisticNumber {
+
+    public static void main(String[] args) {
+    isNarcissistic(153);
+    }
+
+    // region METHOD
+
+    public static boolean isNarcissistic(int number) {
+
+        String numberToString = String.valueOf(number);
+        int totalSumNumber = 0;
+        for (int i = 0; i < numberToString.length() ; i ++)
+        {
+            int individualNumber = Character.getNumericValue( numberToString.charAt(i));
+            totalSumNumber += (int) Math.pow( individualNumber, numberToString.length());
+        }
+        return totalSumNumber == number;
+    }
+
+    // endregion
+
+}
